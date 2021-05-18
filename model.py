90381c3b619d7f41eeedf841a81712a7aa280b62
cha-no <shinchan1996@naver.com>
2021-05-18 15:38:50 +0900

    Add SOMDST

--- model.py
+++ model.py
@@ -8,8 +8,7 @@ import torch.nn.functional as F
 from transformers import ElectraModel
 from torch.nn import CosineEmbeddingLoss, CrossEntropyLoss
 from transformers import BertModel, BertPreTrainedModel
-
-
+from transformers.modeling_bert import BertOnlyMLMHead
 
 def masked_cross_entropy_for_value(logits, target, pad_idx=0):
     mask = target.ne(pad_idx)
@@ -22,7 +21,6 @@ def masked_cross_entropy_for_value(logits, target, pad_idx=0):
     loss = losses.sum() / (mask.sum().float())
     return loss
 
-
 class TRADE(nn.Module):
     def __init__(self, config, tokenized_slot_meta, pad_idx=0):
         super(TRADE, self).__init__()
@@ -34,6 +32,8 @@ class TRADE(nn.Module):
         #     config.proj_dim,
         #     pad_idx,
         # )
+        self.config = config
+        self.tokenized_slot_meta = tokenized_slot_meta
         if config.model_name_or_path:
             self.encoder = BertModel.from_pretrained(config.model_name_or_path)
         else:
@@ -48,7 +48,8 @@ class TRADE(nn.Module):
             pad_idx,
         )
 
-        self.decoder.set_slot_idx(tokenized_slot_meta)
+        self.decoder.set_slot_idx(self.tokenized_slot_meta)
+        self.mlm_head = BertOnlyMLMHead(self.config)
         self.tie_weight()
         
     def set_subword_embedding(self, model_name_or_path):
@@ -79,6 +80,38 @@ class TRADE(nn.Module):
         )
 
         return all_point_outputs, all_gate_outputs
+    
+    @staticmethod
+    def mask_tokens(inputs, tokenizer, config, mlm_probability=0.15):
+        """ Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original. """
+        labels = inputs.clone()
+        # We sample a few tokens in each sequence for masked-LM training (with probability args.mlm_probability defaults to 0.15 in Bert/RoBERTa)
+        probability_matrix = torch.full(labels.shape, mlm_probability).to(inputs.device)
+        #special_tokens_mask = [tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()]
+
+        probability_matrix.masked_fill_(torch.eq(labels, 0), value=0.0)
+
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+        # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+        indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).to(device=inputs.device, dtype=torch.bool) & masked_indices
+        inputs[indices_replaced] = tokenizer.convert_tokens_to_ids(["[MASK]"])[0]
+
+        # 10% of the time, we replace masked input tokens with random word
+        indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).to(device=inputs.device, dtype=torch.bool) & masked_indices & ~indices_replaced
+        random_words = torch.randint(config.vocab_size, labels.shape, device=inputs.device, dtype=torch.long)
+        inputs[indices_random] = random_words[indices_random].to(inputs.device)
+
+        # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+        return inputs, labels
+    
+    def forward_pretrain(self, input_ids, tokenizer):
+        input_ids, labels = self.mask_tokens(input_ids, tokenizer, self.config)
+        encoder_outputs, _ = self.encoder(input_ids=input_ids)
+        mlm_logits = self.mlm_head(encoder_outputs)
+        
+        return mlm_logits, labels
 
 
 class GRUEncoder(nn.Module):
@@ -547,3 +580,136 @@ class SUMBT(nn.Module):
             torch.nn.init.xavier_normal_(module.weight_hh_l0)
             torch.nn.init.constant_(module.bias_ih_l0, 0.0)
             torch.nn.init.constant_(module.bias_hh_l0, 0.0)
+            
+
+class SomDST(nn.Module):
+    def __init__(self, config, n_op, n_domain, update_id, exclude_domain=False):
+        super(SomDST, self).__init__()
+        self.hidden_size = config.hidden_size
+        self.encoder = Encoder(config, n_op, n_domain, update_id, exclude_domain)
+        self.decoder = Decoder(config, self.encoder.bert.embeddings.word_embeddings.weight)
+
+    def forward(self, input_ids, token_type_ids,
+                state_positions, attention_mask,
+                max_value, op_ids=None, max_update=None, teacher=None):
+
+        enc_outputs = self.encoder(input_ids=input_ids,
+                                   token_type_ids=token_type_ids,
+                                   state_positions=state_positions,
+                                   attention_mask=attention_mask,
+                                   op_ids=op_ids,
+                                   max_update=max_update)
+
+        domain_scores, state_scores, decoder_inputs, sequence_output, pooled_output = enc_outputs
+        gen_scores = self.decoder(input_ids, decoder_inputs, sequence_output,
+                                  pooled_output, max_value, teacher)
+
+        return domain_scores, state_scores, gen_scores
+
+
+class Encoder(nn.Module):
+    def __init__(self, config, n_op, n_domain, update_id, exclude_domain=False):
+        super(Encoder, self).__init__()
+        self.hidden_size = config.hidden_size
+        self.exclude_domain = exclude_domain
+        self.bert = BertModel.from_pretrained(config.model_name_or_path)
+        self.bert.resize_token_embeddings(config.vocab_size)
+        self.dropout = nn.Dropout(config.dropout)
+        self.action_cls = nn.Linear(config.hidden_size, n_op)
+        if self.exclude_domain is not True:
+            self.domain_cls = nn.Linear(config.hidden_size, n_domain)
+        self.n_op = n_op
+        self.n_domain = n_domain
+        self.update_id = update_id
+
+    def forward(self, input_ids, token_type_ids,
+                state_positions, attention_mask,
+                op_ids=None, max_update=None):
+        bert_outputs = self.bert(input_ids, token_type_ids, attention_mask)
+        sequence_output, pooled_output = bert_outputs[:2]
+        state_pos = state_positions[:, :, None].expand(-1, -1, sequence_output.size(-1))
+        state_output = torch.gather(sequence_output, 1, state_pos)
+        state_scores = self.action_cls(self.dropout(state_output))  # B,J,4
+        if self.exclude_domain:
+            domain_scores = torch.zeros(1, device=input_ids.device)  # dummy
+        else:
+            domain_scores = self.domain_cls(self.dropout(pooled_output))
+
+        batch_size = state_scores.size(0)
+        if op_ids is None:
+            op_ids = state_scores.view(-1, self.n_op).max(-1)[-1].view(batch_size, -1)
+        if max_update is None:
+            max_update = op_ids.eq(self.update_id).sum(-1).max().item()
+
+        gathered = []
+        for b, a in zip(state_output, op_ids.eq(self.update_id)):  # update
+            if a.sum().item() != 0:
+                v = b.masked_select(a.unsqueeze(-1)).view(1, -1, self.hidden_size)
+                n = v.size(1)
+                gap = max_update - n
+                if gap > 0:
+                    zeros = torch.zeros(1, 1*gap, self.hidden_size, device=input_ids.device)
+                    v = torch.cat([v, zeros], 1)
+            else:
+                v = torch.zeros(1, max_update, self.hidden_size, device=input_ids.device)
+            gathered.append(v)
+        decoder_inputs = torch.cat(gathered)
+        return domain_scores, state_scores, decoder_inputs, sequence_output, pooled_output.unsqueeze(0)
+
+
+class Decoder(nn.Module):
+    def __init__(self, config, bert_model_embedding_weights):
+        super(Decoder, self).__init__()
+        self.pad_idx = 0
+        self.hidden_size = config.hidden_size
+        self.vocab_size = config.vocab_size
+        self.embed = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=self.pad_idx)
+        self.embed.weight = bert_model_embedding_weights
+        self.gru = nn.GRU(config.hidden_size, config.hidden_size, 1, batch_first=True)
+        self.w_gen = nn.Linear(config.hidden_size*3, 1)
+        self.sigmoid = nn.Sigmoid()
+        self.dropout = nn.Dropout(config.dropout)
+
+        for n, p in self.gru.named_parameters():
+            if 'weight' in n:
+                p.data.normal_(mean=0.0, std=config.initializer_range)
+
+    def forward(self, x, decoder_input, encoder_output, hidden, max_len, teacher=None):
+        mask = x.eq(self.pad_idx)
+        batch_size, n_update, _ = decoder_input.size()  # B,J',5 # long
+        state_in = decoder_input
+        all_point_outputs = torch.zeros(n_update, batch_size, max_len, self.vocab_size).to(x.device)
+        result_dict = {}
+        for j in range(n_update):
+            w = state_in[:, j].unsqueeze(1)  # B,1,D
+            slot_value = []
+            for k in range(max_len):
+                w = self.dropout(w)
+                _, hidden = self.gru(w, hidden)  # 1,B,D
+                # B,T,D * B,D,1 => B,T
+                attn_e = torch.bmm(encoder_output, hidden.permute(1, 2, 0))  # B,T,1
+                attn_e = attn_e.squeeze(-1).masked_fill(mask, -1e9)
+                attn_history = nn.functional.softmax(attn_e, -1)  # B,T
+
+                # B,D * D,V => B,V
+                attn_v = torch.matmul(hidden.squeeze(0), self.embed.weight.transpose(0, 1))  # B,V
+                attn_vocab = nn.functional.softmax(attn_v, -1)
+
+                # B,1,T * B,T,D => B,1,D
+                context = torch.bmm(attn_history.unsqueeze(1), encoder_output)  # B,1,D
+
+                p_gen = self.sigmoid(self.w_gen(torch.cat([w, hidden.transpose(0, 1), context], -1)))  # B,1
+                p_gen = p_gen.squeeze(-1)
+
+                p_context_ptr = torch.zeros_like(attn_vocab).to(x.device)
+                p_context_ptr.scatter_add_(1, x, attn_history)  # copy B,V
+                p_final = p_gen * attn_vocab + (1 - p_gen) * p_context_ptr  # B,V
+                _, w_idx = p_final.max(-1)
+                slot_value.append([ww.tolist() for ww in w_idx])
+                if teacher is not None:
+                    w = self.embed(teacher[:, j, k]).unsqueeze(1)
+                else:
+                    w = self.embed(w_idx).unsqueeze(1)  # B,1,D
+                all_point_outputs[j, :, k, :] = p_final
+
+        return all_point_outputs.transpose(0, 1)
