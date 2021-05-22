c92b0351014eb60f65435e384baba8a6059ca210
cha-no <shinchan1996@naver.com>
2021-05-22 11:46:40 +0900

    preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,7 +1,17 @@
-import numpy as np
-import torch
+"""preprocessor of dst tasks
 
-from data_utils import (
+    TRADE, SUMBT, SOMDST
+
+"""
+
+from typing import List, Dict, Tuple
+import numpy as np
+
+import torch
+from transformers import BertTokenizer
+
+from utils.data_utils import (
+    DSTInputExample,
     DSTPreprocessor,
     OpenVocabDSTFeature,
     convert_state_dict,
@@ -16,26 +26,50 @@ flatten = lambda x: [i for s in x for i in s]
 domain2id = {d: i for i, d in enumerate(EXPERIMENT_DOMAINS)}
 
 class TRADEPreprocessor(DSTPreprocessor):
+    """TRADE Processor
+
+    Args:
+        DSTPreprocessor: Abstract class
+    """
     def __init__(
-        self,
-        slot_meta,
-        src_tokenizer,
-        trg_tokenizer=None,
-        ontology=None,
-        max_seq_length=512,
-        word_dropout=0.0,
-    ):
+        self : DSTPreprocessor,
+        slot_meta : List[str],
+        src_tokenizer : BertTokenizer,
+        trg_tokenizer : BertTokenizer = None,
+        ontology : Dict[str, List[str]] = None,
+        max_seq_length : int = 512,
+        word_dropout : float = 0.0,
+    ) -> None:
+        """Initialize TRADE Preprocessor
+
+        Args:
+            self (DSTPreprocessor)
+            slot_meta (List[str])
+            src_tokenizer (BertTokenizer): source tokenizer
+            trg_tokenizer (BertTokenizer, optional): target tokenizer. Defaults to None.
+            ontology (Dict[str, List[str]], optional). Defaults to None.
+            max_seq_length (int, optional): max sequence length. Defaults to 512.
+            word_dropout (float, optional): word dropout. Defaults to 0.0.
+        """
         self.slot_meta = slot_meta
         self.src_tokenizer = src_tokenizer
         self.trg_tokenizer = trg_tokenizer if trg_tokenizer else src_tokenizer
         self.ontology = ontology
-        # self.gating2id = {"none": 0, "dontcare": 1, "ptr": 2}
         self.gating2id = {"none": 0, "dontcare": 1, "yes": 2, "no": 3, "ptr": 4}
         self.id2gating = {v: k for k, v in self.gating2id.items()}
         self.max_seq_length = max_seq_length
         self.word_dropout = word_dropout
 
-    def _convert_example_to_feature(self, example):
+    def _convert_example_to_feature(self : DSTPreprocessor, example : DSTInputExample) -> OpenVocabDSTFeature:
+        """Convert example to feature
+
+        Args:
+            self (DSTPreprocessor)
+            example (DSTInputExample): DSTInputExample
+
+        Returns:
+            OpenVocabDSTFeature:
+        """
         dialogue_context = " [SEP] ".join(example.context_turns + example.current_turn)
 
         input_id = self.src_tokenizer.encode(dialogue_context, add_special_tokens=False)
@@ -69,10 +103,29 @@ class TRADEPreprocessor(DSTPreprocessor):
             example.guid, input_id, segment_id, gating_id, target_ids
         )
 
-    def convert_examples_to_features(self, examples):
+    def convert_examples_to_features(self : DSTPreprocessor, examples : List[DSTInputExample]) -> List[OpenVocabDSTFeature]:
+        """Convert example to features
+
+        Args:
+            self (DSTPreprocessor)
+            examples (List[DSTInputExample]): List[example[0], example[1], ...]
+
+        Returns:
+            List[OpenVocabDSTFeature]: List[OpenVocabDSTFeature[0], OpenVocabDSTFeature[1], ...]
+        """
         return list(map(self._convert_example_to_feature, examples))
 
-    def recover_state(self, gate_list, gen_list):
+    def recover_state(self : DSTPreprocessor, gate_list : List, gen_list : List) -> List[str]:
+        """Recover state
+
+        Args:
+            self (DSTPreprocessor)
+            gate_list (List): gate predictions
+            gen_list (List): generate predictions
+
+        Returns:
+            List[str]: state predictions
+        """
         assert len(gate_list) == len(self.slot_meta)
         assert len(gen_list) == len(self.slot_meta)
 
@@ -84,10 +137,6 @@ class TRADEPreprocessor(DSTPreprocessor):
             if self.id2gating[gate] in ["dontcare", "yes", "no"]:
                 recovered.append("%s-%s" % (slot, self.id2gating[gate]))
                 continue
-
-            # if self.id2gating[gate] == "dontcare":
-            #     recovered.append("%s-%s" % (slot, "dontcare"))
-            #     continue
 
             token_id_list = []
             for id_ in value:
@@ -103,7 +152,16 @@ class TRADEPreprocessor(DSTPreprocessor):
             recovered.append("%s-%s" % (slot, value))
         return recovered
 
-    def collate_fn(self, batch):
+    def collate_fn(self : DSTPreprocessor, batch : List) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        """pytorch collate fn
+
+        Args:
+            self (DSTPreprocessor)
+            batch (List): batch of DataLoader
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]: (input_ids, segment_ids, input_masks, gating_ids, target_ids, guids)
+        """
         guids = [b.guid for b in batch]
 
         if self.word_dropout > 0.0:
@@ -138,16 +196,33 @@ class TRADEPreprocessor(DSTPreprocessor):
 
 
 class SUMBTPreprocessor(DSTPreprocessor):
+    """SUMBT Processor
+
+    Args:
+        DSTPreprocessor: Abstract class
+    """
     def __init__(
-        self,
-        slot_meta,
-        src_tokenizer,
-        trg_tokenizer=None,
-        ontology=None,
-        max_seq_length=64,
-        max_turn_length=14,
-        word_dropout=0.0,
+        self : DSTPreprocessor,
+        slot_meta : List[str],
+        src_tokenizer : BertTokenizer,
+        trg_tokenizer : BertTokenizer=None,
+        ontology : Dict[str, List[str]] = None,
+        max_seq_length : int = 64,
+        max_turn_length : int = 14,
+        word_dropout : float = 0.0,
     ):
+        """Initialize SUMBT Processor
+
+        Args:
+            self (DSTPreprocessor)
+            slot_meta (List[str])
+            src_tokenizer (BertTokenizer): source tokenizer
+            trg_tokenizer (BertTokenizer, optional): target tokenizer. Defaults to None.
+            ontology (Dict[str, List[str]], optional). Defaults to None.
+            max_seq_length (int, optional): max sequence length. Defaults to 64.
+            max_turn_length (int, optional): max turn of dialogues. Defaults to 14.
+            word_dropout (float, optional). Defaults to 0.0.
+        """
         self.slot_meta = slot_meta
         self.src_tokenizer = src_tokenizer
         self.trg_tokenizer = trg_tokenizer if trg_tokenizer else src_tokenizer
@@ -156,7 +231,16 @@ class SUMBTPreprocessor(DSTPreprocessor):
         self.max_turn_length = max_turn_length
         self.word_dropout = word_dropout
 
-    def _convert_example_to_feature(self, example):
+    def _convert_example_to_feature(self : DSTPreprocessor, example : DSTInputExample) -> OntologyDSTFeature:
+        """Convert example to feature
+
+        Args:
+            self (DSTPreprocessor)
+            example (DSTInputExample): DSTInputExample
+
+        Returns:
+            OntologyDSTFeature:
+        """
         guid = example[0].guid.rsplit("-", 1)[0]  # dialogue_idx
         turns = []
         token_types = []
@@ -215,10 +299,30 @@ class SUMBTPreprocessor(DSTPreprocessor):
             target_ids=labels,
         )
 
-    def convert_examples_to_features(self, examples):
+    def convert_examples_to_features(self : DSTPreprocessor, examples : List[DSTInputExample]) -> List[OntologyDSTFeature]:
+        """Convert example to features
+
+        Args:
+            self (DSTPreprocessor)
+            examples (List[DSTInputExample]): List[example[0], example[1], ...]
+
+        Returns:
+            List[OntologyDSTFeature]: List[OntologyDSTFeature[0], OntologyDSTFeature[1], ...]
+        """
         return list(map(self._convert_example_to_feature, examples))
 
-    def recover_state(self, pred_slots, num_turn):
+    def recover_state(self : DSTPreprocessor, pred_slots : List[int], num_turn : int) -> List[str]:
+        """Recover state
+
+        Args:
+            self (DSTPreprocessor)
+            pred_slots (List[int]): slot's pred id
+            num_turn (int): current turn
+
+        Returns:
+            List[str]: state prediction
+        """
+
         states = []
         for pred_slot in pred_slots[:num_turn]:
             state = []
@@ -229,17 +333,26 @@ class SUMBTPreprocessor(DSTPreprocessor):
             states.append(state)
         return states
 
-    def collate_fn(self, batch):
+    def collate_fn(self : DSTPreprocessor, batch : List) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        """pytorch collate fn
+
+        Args:
+            self (DSTPreprocessor)
+            batch (List): batch of DataLoader
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]: (input_ids, segment_ids, input_masks, target_ids, num_turns, guids)
+        """
         guids = [b.guid for b in batch]
 
         if self.word_dropout > 0.0:
             input_ids = []
             for b in batch:
-                drop_mask = (np.array(self.src_tokenizer.get_special_tokens_mask(b.input_id, already_has_special_tokens=True)) == 0).astype(int)
+                drop_mask = (np.array(self.src_tokenizer.get_special_tokens_mask(b.input_ids, already_has_special_tokens=True)) == 0).astype(int)
                 word_drop = np.random.binomial(drop_mask, self.word_dropout)
                 input_id = [
                     token_id if word_drop[i] == 0 else self.src_tokenizer.unk_token_id
-                    for i, token_id in enumerate(b.input_id)
+                    for i, token_id in enumerate(b.input_ids)
                 ]
                 input_ids.append(input_id) 
             input_ids = torch.LongTensor(
@@ -247,17 +360,31 @@ class SUMBTPreprocessor(DSTPreprocessor):
             )
         else:
             input_ids = torch.LongTensor(
-                self.pad_ids([b.input_id for b in batch], self.src_tokenizer.pad_token_id)
+                self.pad_ids([b.input_ids for b in batch], self.src_tokenizer.pad_token_id)
             )
 
-        # input_ids = torch.LongTensor([b.input_ids for b in batch])
         segment_ids = torch.LongTensor([b.segment_ids for b in batch])
         input_masks = input_ids.ne(self.src_tokenizer.pad_token_id)
         target_ids = torch.LongTensor([b.target_ids for b in batch])
         num_turns = [b.num_turn for b in batch]
         return input_ids, segment_ids, input_masks, target_ids, num_turns, guids
 
-def tokenize_ontology(ontology, tokenizer, max_seq_length = 12):
+def tokenize_ontology(
+    ontology : Dict[str, List[str]], 
+    tokenizer : BertTokenizer, 
+    max_seq_length : int = 12
+) -> Tuple[torch.Tensor, List[torch.Tensor]]:
+    """Tokenize Ontology
+
+    Args:
+        ontology (Dict[str, List[str]])
+        tokenizer (BertTokenizer)
+        max_seq_length (int, optional): max sequence(slot, value) length. Defaults to 12.
+
+    Returns:
+        Tuple[torch.Tensor, List[torch.Tensor]]: (slot token tensor, List[value token tensor])
+    """
+
     slot_types, slot_values = [], []
     
     for (slot, values) in ontology.items():
@@ -278,19 +405,39 @@ def tokenize_ontology(ontology, tokenizer, max_seq_length = 12):
     return torch.LongTensor(slot_types), slot_values
         
 class SOMDSTPreprocessor(DSTPreprocessor):
+    """SOMDST Preprocessor
+
+    Args:
+        DSTPreprocessor
+    """
     def __init__(
-        self,
-        slot_meta,
-        src_tokenizer,
-        trg_tokenizer=None,
-        n_history = 1,
-        slot_token = '[SLOT]',
-        separate_token = ';',
-        ontology=None,
-        max_seq_length=512,
-        word_dropout=0.0,
-        op_code='4'
+        self : DSTPreprocessor,
+        slot_meta : List[str],
+        src_tokenizer : BertTokenizer,
+        trg_tokenizer : BertTokenizer = None,
+        n_history : int = 1,
+        slot_token : str = '[SLOT]',
+        separate_token : str = ';',
+        ontology : Dict[str, List[str]] = None,
+        max_seq_length : int = 512,
+        word_dropout : float = 0.0,
+        op_code : str = '4'
     ):
+        """Initialize SOMDST Preprocessor
+
+        Args:
+            self (DSTPreprocessor)
+            slot_meta (List[str])
+            src_tokenizer (BertTokenizer)
+            trg_tokenizer (BertTokenizer, optional)
+            n_history (int, optional): Consider n_history last dialogue. Defaults to 1.
+            slot_token (str, optional): Special slot token. Defaults to '[SLOT]'.
+            separate_token (str, optional): Seperate token. Defaults to ';'.
+            ontology (Dict[str, List[str]], optional): Defaults to None.
+            max_seq_length (int, optional): Defaults to 512.
+            word_dropout (float, optional): Defaults to 0.0.
+            op_code (str, optional): Defaults to '4'.
+        """
         self.slot_meta = slot_meta
         self.src_tokenizer = src_tokenizer
         self.trg_tokenizer = trg_tokenizer if trg_tokenizer else src_tokenizer
@@ -304,13 +451,24 @@ class SOMDSTPreprocessor(DSTPreprocessor):
         self.op_ids = None
         self.generate_ids = None        
         
+        # special token을 추가함
         special_tokens_dict = {'additional_special_tokens': [slot_token, '[NULL]', '[EOS]']}
         num_added_toks = self.src_tokenizer.add_special_tokens(special_tokens_dict)
         
-    def _convert_example_to_feature(self, example):
+    def _convert_example_to_feature(self : DSTPreprocessor, example : DSTInputExample) -> SOMDSTFeature:
+        """Convert DSTInputExample to SOMDSTFeature
+
+        Args:
+            self (DSTPreprocessor)
+            example (List[DSTInputExample])
+
+        Returns:
+            SOMDSTFeature
+        """
         max_seq_length = self.max_seq_length
         state = []
         
+        # 모든 slot에 대해 이전 turn의 value를 구해줌 -> 모델 input으로 들어감
         for s in self.slot_meta:
             state.append(self.slot_token)
             k = s.split('-')
@@ -323,9 +481,11 @@ class SOMDSTPreprocessor(DSTPreprocessor):
                 t.extend(['-', '[NULL]'])
             state.extend(t)
         
+        # 이전 1개의 발화만 고려
         dialog_history = self.separate_token.join(example.context_turns[-2 * self.n_history:])
         turn_utter = self.separate_token.join(example.current_turn)
         
+        # max_seq_len만큼 길이 맞춰줌
         avail_length_1 = max_seq_length - len(state) - 3
         diag_1 = self.src_tokenizer.tokenize(dialog_history)
         diag_2 = self.src_tokenizer.tokenize(turn_utter)
@@ -345,6 +505,7 @@ class SOMDSTPreprocessor(DSTPreprocessor):
         segment = [0] * len(diag_1) + [1] * len(diag_2)
 
         diag = diag_1 + diag_2
+
         # word dropout
         if self.word_dropout > 0.:
             drop_mask = np.array(drop_mask)
@@ -354,8 +515,9 @@ class SOMDSTPreprocessor(DSTPreprocessor):
         input_ = diag + state
         segment = segment + [1] * len(state)
         self.input_ = input_
-
         self.segment_id = segment
+
+        # input_에서 slot의 position을 구해줌
         slot_position = []
         for i, t in enumerate(self.input_):
             if t == self.slot_token:
@@ -364,6 +526,8 @@ class SOMDSTPreprocessor(DSTPreprocessor):
 
         input_mask = [1] * len(self.input_)
         self.input_id = self.src_tokenizer.convert_tokens_to_ids(self.input_)
+        
+        # padding
         if len(input_mask) < max_seq_length:
             self.input_id = self.input_id + [0] * (max_seq_length-len(input_mask))
             self.segment_id = self.segment_id + [0] * (max_seq_length-len(input_mask))
@@ -371,8 +535,12 @@ class SOMDSTPreprocessor(DSTPreprocessor):
 
         self.input_mask = input_mask
         self.domain_id = domain2id[example.turn_domain]
+
+        # train set, validation set에 대해서 operation label을 operation id로 바꿔줌
         if example.op_labels:
             self.op_ids = [self.op2id[a] for a in example.op_labels]
+
+        # train set, validation set에 대해서 generate_y를 generate_y로 바꿔줌
         if example.generate_y:
             self.generate_ids = [self.src_tokenizer.convert_tokens_to_ids(y) for y in example.generate_y]
         else:
@@ -383,17 +551,44 @@ class SOMDSTPreprocessor(DSTPreprocessor):
         )
 
 
-    def convert_examples_to_features(self, examples):
+    def convert_examples_to_features(self : DSTPreprocessor, examples : List[DSTInputExample]) -> List[SOMDSTFeature]:
+        """Convert examples to features
+
+        Args:
+            self (DSTPreprocessor)
+            examples (List[DSTInputExample])
+
+        Returns:
+            List[SOMDSTFeature]
+        """
         return list(map(self._convert_example_to_feature, examples))
 
-    def check_testdata(self, batch):
+    def check_testdata(self : DSTPreprocessor, batch : List) -> bool:
+        """Check Test data
+
+        Args:
+            self (DSTPreprocessor)
+            batch (List)
+
+        Returns:
+            bool: Decide Test Data
+        """
         for b in batch:
             if b.op_ids is not None:
                 return True
             else:
                 return False
         
-    def collate_fn(self, batch):
+    def collate_fn(self : DSTPreprocessor, batch : List) -> Tuple:
+        """collate_fn
+
+        Args:
+            self (DSTPreprocessor)
+            batch (List)
+
+        Returns:
+            Tuple: Convert tensor
+        """
         input_ids = torch.tensor([b.input_id for b in batch], dtype=torch.long)
         input_mask = torch.tensor([b.input_mask for b in batch], dtype=torch.long)
         segment_ids = torch.tensor([b.segment_id for b in batch], dtype=torch.long)
